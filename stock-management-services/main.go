0cde52b74555e0031e053618cfff5b05a78d80dc
Omar Elmasaoudi <omarelmasaoudi04@gmail.com>
2024-02-27 10:00:40 -0500

    middleware module added

--- stock-management-services/main.go
+++ stock-management-services/main.go
@@ -1,6 +1,19 @@
 package main
-import ("fmt") 
 
-func main(){
-	fmt.Println("Hello world")
+import (
+	"fmt"
+	"github.com/Omare04/medApp.git/stock-management-services/middlewarte"
+	// "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// app := fiber.New()
+
+	// app.Get("/", func(c *fiber.Ctx) error {
+	// 	return c.SendString("Hello, World!")
+	// })
+
+	// app.Listen(":3000")
+	fmt.Printf("HI winner")
+	middleware.TestMiddleware()
 }
