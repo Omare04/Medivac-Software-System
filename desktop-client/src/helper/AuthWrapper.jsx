6456ad47dc19c8487c1ec8f6ec66e8ba94b07187
Omar Elmasaoudi <omarelmasaoudi04@gmail.com>
2024-03-17 22:50:20 -0400

    Medical Equipment stock ui added

--- desktop-client/src/helper/AuthWrapper.jsx
+++ desktop-client/src/helper/AuthWrapper.jsx
@@ -9,21 +9,23 @@ import React, {
 import { Home } from "../Pages/Home";
 import Map from "../Pages/Map";
 import MedicalEquipmentStock from "../Pages/Stock/MedicalEquipmentStock";
-import Personele from "../Pages/Personele/Personele";
+import Personele from "../Pages/Personele/Personnel";
 import Missions from "../Pages/Missions";
 import Header from "../Layout/Header";
 import { Divider, Flex } from "@chakra-ui/react";
+import OrderPageMain from "../Pages/Orders/OrderPageMain";
 
 export function ProtectedRoutes() {
   return (
     <>
       <Header />
-      <div style={{ height: "calc(100% - (50px ))", padding: "10px" }}>
+      <div style={{ height: "calc(100% - (50px))", padding: "10px" }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Map" element={<Map />} />
-          <Route path="/stock" element={<MedicalEquipmentStock />} />
-          <Route path="/personele" element={<Personele />} />
+          <Route path="/Inventory" element={<MedicalEquipmentStock />} />
+          <Route path="/orders" element={<OrderPageMain />} />
+          <Route path="/personnel" element={<Personele />} />
           <Route path="/Missions" element={<Missions />} />
         </Routes>
       </div>
